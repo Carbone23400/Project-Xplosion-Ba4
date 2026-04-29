9f36847f720c694b78529cc7497241d203662399
Claire-Bachmann <claire.bachmann@epfl.ch>
2026-04-29 10:35:41 +0200

    final move of coordchem and added notebook folder

--- coordchem/__init__.py
+++
@@ -1,5 +0,0 @@
-from .parser import parse_formula, ParsedComplex, FormulaParseError
-from .geometry import get_geometry, predict_geometry, geometry_report, get_d_count
-
-__all__ = ["parse_formula", "ParsedComplex", "FormulaParseError", "get_geometry", "predict_geometry", "geometry_report"]
-__version__ = "0.1.0"
