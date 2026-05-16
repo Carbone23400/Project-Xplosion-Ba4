941f0fceedb7db96a7f0257c2d45b66b889a7dfa
Claire-Bachmann <claire.bachmann@epfl.ch>
2026-05-16 09:20:52 +0200

    rep

--- src/spectra/predictor.py
+++
@@ -1,5 +0,0 @@
-"""Compatibility wrapper for the historical ``src.spectra.predictor`` path."""
-
-from coordchem.spectra.predictor import *  # noqa: F401,F403
-from coordchem.spectra.predictor import _scale_intensity
-
